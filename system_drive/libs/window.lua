4d497d31b33212f3ee2279e8c808b981a8f91a85
poetic android <i@poeticandroid.online>
2019-09-20 07:05:41 +0200

    I made a class class

--- system_drive/libs/window.lua
+++ system_drive/libs/window.lua
@@ -1,43 +1,53 @@
+Class = require("class")
+
 local defaultfont = text.loadfont("Victoria.8b")
 
 local Window = {}
 do
-  Window.__index = Window
-
-  function Window:new(parent, title, left,top, width,height)
-    local self = setmetatable({}, Window)
-    self.rootvp = view.new(parent, left,top, width,height)
+  function Window:new(parent, title, left, top, width, height)
+    self.rootvp = view.new(parent, left, top, width, height)
     self.mainvp = view.new(self.rootvp, 0, 8, 8, 8)
-    self._resbtn= view.new(self.rootvp, 0, 8, 8, 8)
+    self._resbtn = view.new(self.rootvp, 0, 8, 8, 8)
     view.active(self.mainvp)
-    self:colors(1, 0)
     self:font(defaultfont)
+    self:colors(1, 2, 3, 0)
     self:title(title)
-    return self
   end
-  
-  function Window:colors(bg, fg)
-    if bg then
-      self._bgcolor = bg
-      self._fgcolor = fg
-      self:title(self._title)
+  Window = Class:new(nil, Window.new)
+
+  function Window:colors(shine, shadow, focus, blur)
+    if blur then
+      self._shinec = shine
+      self._shadowc = shadow
+      self._focusc = focus
+      self._blurc = blur
+      self:redraw()
     end
-    return self._bgcolor, self._fgcolor
   end
 
   function Window:redraw()
     local prevvp = view.active()
+    local focused = view.focused(self.rootvp)
     view.active(self.rootvp)
     local sw, sh = view.size(self.rootvp)
     local tw, th = text.draw(self._title, self._font, 0, 0)
     self._titleheight = th
-    view.position(self.mainvp, 1, th + 2)
-    view.size(self.mainvp, sw-2, sh - th - 3)
-    view.position(self._resbtn, sw-8,sh-8)
-    self:title(self._title)
-    view.active(self._resbtn)
-    gfx.bgcolor(self._bgcolor)
+    view.position(self.mainvp, 3, th + 3)
+    view.size(self.mainvp, sw - 6, sh - th - 6)
+    view.position(self._resbtn, sw - 8, sh - 8)
+    gfx.bgcolor(focused and self._focusc or self._blurc)
     gfx.cls()
+    gfx.fgcolor(self._shadowc)
+    text.draw(self._title, self._font, 2, 2)
+    self:outset(0, 0, sw, sh)
+    self:inset(2, th + 2, sw - 4, sh - th - 4)
+    view.active(self._resbtn)
+    gfx.bgcolor(focused and self._focusc or self._blurc)
+    gfx.cls()
+    self:outset(0, 0, 10, 10)
+    self:outset(-2, -2, 10, 10)
+    gfx.pixel(0, 6, gfx.pixel(1, 6))
+    gfx.pixel(6, 0, gfx.pixel(6, 1))
     view.active(prevvp)
   end
 
@@ -52,14 +62,7 @@ do
   function Window:title(title)
     if title then
       self._title = view.attribute(self.rootvp, "title", title)
-      local prevvp = view.active()
-      view.active(self.rootvp)
-      gfx.bgcolor(self._bgcolor)
-      gfx.fgcolor(self._fgcolor)
-      gfx.cls()
-      local vw, vh = view.size(self.rootvp)
-      local tw, th = text.draw(self._title, self._font, 1, 1)
-      view.active(prevvp)
+      self:redraw()
     end
     return self._title
   end
@@ -71,33 +74,35 @@ do
     local x, y, btn = input.mouse()
     if self._lastmbtn == 0 and btn == 1 then
       if y < self._titleheight then
-        self._moving=true
-        self._movingx=x
-        self._movingy=y
+        self._moving = true
+        self._movingx = x
+        self._movingy = y
         view.zindex(self.rootvp, -1)
       end
     end
     if btn == 1 then
       if self._moving then
-        local left,top = self:position()
-        self:position(left +x -self._movingx, top + y -self._movingy)
+        local left, top = self:position()
+        self:position(left + x - self._movingx, top + y - self._movingy)
       end
-    elseif view.focused(self.rootvp) then
-      self._moving=false
-      self:title(self._title)
-      view.focused(self.mainvp, true)
+    else
+      self._moving = false
+    end
+    if self._focused ~= view.focused(self.rootvp) then
+      self._focused = view.focused(self.rootvp)
+      self:redraw()
     end
     self._lastmbtn = btn
     view.active(self._resbtn)
-    local _x,_y,btn = input.mouse()
+    local _x, _y, btn = input.mouse()
     if btn == 1 then
-      self:size(x+4,y+4)
+      self:size(x + 4, y + 4)
       self:redraw()
     end
     view.active(prevvp)
   end
 
-  function Window:position(left,top)
+  function Window:position(left, top)
     if top then
       view.position(self.rootvp, left, top)
     end
@@ -130,9 +135,25 @@ do
         fg = c
       end
     end
-    self:colors(bg, fg)
+    self:colors(bg, fg, 3, 0)
     view.active(prevvp)
   end
 
+  function Window:outset(x, y, w, h)
+    gfx.fgcolor(self._shinec)
+    gfx.bar(x, y, w, 1)
+    gfx.bar(x, y, 1, h)
+    gfx.fgcolor(self._shadowc)
+    gfx.bar(x + w - 1, y + 1, 1, h - 1)
+    gfx.bar(x, y + h - 1, w, 1)
+  end
+  function Window:inset(x, y, w, h)
+    gfx.fgcolor(self._shadowc)
+    gfx.bar(x, y, w, 1)
+    gfx.bar(x, y, 1, h)
+    gfx.fgcolor(self._shinec)
+    gfx.bar(x + w - 1, y + 1, 1, h - 1)
+    gfx.bar(x, y + h - 1, w, 1)
+  end
 end
 return Window
