3b16d4b46f1c95b1f379f8867e2de6f84f0ebc10
poetic android <i@poeticandroid.online>
2020-03-25 12:14:34 +0100

    open markdown files with `more`

--- system_drive/cmd/open.lua
+++ system_drive/cmd/open.lua
@@ -27,6 +27,8 @@ function _init(args)
       task = sys.startchild(_DRIVE .. "cmd/show.lua", {openfile})
     elseif ext == "wav" then
       task = sys.startchild(_DRIVE .. "cmd/play.lua", {openfile})
+    elseif ext == "md" then
+      task = sys.startchild(_DRIVE .. "cmd/more.lua", {openfile})
     elseif ext == "mod" then
       task = sys.startchild(_DRIVE .. "cmd/playmod.lua", {openfile})
     else
