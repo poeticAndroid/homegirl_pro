655cac843c6220a86cc01ed30aff99ea30dd4a50
poetic android <i@poeticandroid.online>
2020-03-25 12:10:51 +0100

    Upgrade to Homegirl 1.4.3

--- system_drive/cmd/fileinfo.lua
+++ system_drive/cmd/fileinfo.lua
@@ -3,7 +3,7 @@ months = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", 
 function _init(args)
   print("Filename: " .. args[1])
   size = fs.size(args[1])
-  print("Size: " .. friendly(size))
+  print("Size: " .. size .. " bytes")
   th, tm, ts, tu = fs.time(args[1])
   dy, dm, dd, dw = fs.date(args[1])
   print(
@@ -19,15 +19,4 @@ function _init(args)
       tu / 60
     )
   )
-end
-
-function friendly(bytes)
-  local units = bytes
-  local measures = {"YiB", "ZiB", "EiB", "PiB", "TiB", "GiB", "MiB", "KiB"}
-  local measure = "bytes"
-  while units >= 1024 do
-    units = units / 1024
-    measure = table.remove(measures) .. " (" .. bytes .. " bytes)"
-  end
-  return string.format("%4.3f", units) .. " " .. measure
 end
