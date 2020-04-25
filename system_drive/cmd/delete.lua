5d16209e5ab5981f81faa418bcbb35ca2a023a48
poetic android <i@poeticandroid.online>
2020-04-25 17:40:19 +0200

    Upgrade to Homegirl 1.5.5

--- system_drive/cmd/delete.lua
+++ system_drive/cmd/delete.lua
@@ -1,8 +1,10 @@
 function _init(args)
-  if not args[1] then
-    return print("..delete what?")
+  if #args < 1 then
+    print("Usage: delete <path>")
+    return sys.exit(1)
   end
   if not fs.delete(args[1]) then
     print("Could not delete '" .. args[1] .. "'!")
+    return sys.exit(1)
   end
 end
